6a195c98aa2120e4d8edbee11d789efaf91e1d24
Oron Paz <oron.paz26@gmail.com>
2023-11-30 17:30:49 -0800

    slight change

---
+++ client.cpp
@@ -0,0 +1,15 @@
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <iostream>
+#include <unistd.h>
+#define PORT 8080
+
+int main(int argc, char const* argv[]) {
+  int listen();
+
+
+  return 0;
+}
