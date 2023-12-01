0a49052f90f23f7ffbc35f0fe401bb72374f5eaf
Oron Paz <oron.paz26@gmail.com>
2023-12-01 12:04:49 -0800

    have outline for client side and connection established but not working as intended

--- client.cpp
+++ client.cpp
@@ -1,15 +1,75 @@
-#include <netinet/in.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
 #include <iostream>
+#include <string>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
 #include <unistd.h>
-#define PORT 8080
+#include <string.h>
+#include <netdb.h>
+#include <sys/uio.h>
+#include <sys/time.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <fstream>
+#define PORT 8222
 
-int main(int argc, char const* argv[]) {
-  int listen();
+int main(int argc, char *argv[])
+{
+    char msg[1500];  //buffer for message
+    int sockfdClient = socket(AF_INET, SOCK_STREAM, 0); //creating a socket for the client
+    if (sockfdClient < 0) {
+        std::cerr << "Error creating the socket" << std::endl;
+        return -1;
+    }
 
+    struct sockaddr_in serverAddr;  //writing the server adress adress
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(PORT);
+    serverAddr.sin_addr.s_addr = INADDR_ANY; 
+    sockaddr_in sendSockAddr;   
 
-  return 0;
+    //connecting and checking connection with server -----//
+    if (connect(sockfdClient, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) { 
+        std::cerr << "Error connecting to the server" << std::endl;
+        close(sockfdClient);
+        return -1;
+    }
+    std::cout << "Connected to the server!" << std::endl;
+
+    //recieving and sending messages ----//
+    while (true) {
+        // Get user input
+        std::cout << ">";
+        std::string data;
+        getline(std::cin, data);
+
+        // Send the message to the server
+        send(sockfdClient, data.c_str(), data.length(), 0);
+        // Check for exit condition
+        if (data == "exit") {
+            break;
+        }
+
+        // Receive and display the server's response
+        memset(&msg, 0, sizeof(msg));  // Clear the buffer
+        recv(sockfdClient, msg, sizeof(msg), 0);
+
+        std::cout << "Server: " << msg << std::endl;
+
+        // Check for exit condition ----//
+        if (!strcmp(msg, "exit")) {
+            std::cout << "Server has closed the connection" << std::endl;
+            break;
+        }
+    }
+
+    // Close the socket ----//
+    close(sockfdClient);
+
+    std::cout << "Connection closed..." << std::endl;
+
+    return 0;
 }
--- server.cpp
+++ server.cpp
@@ -31,12 +31,45 @@ int main(int argc, char const* argv[]) {
   //------- LISTEN FOR CONNECTIONS FROM CLIENT SIDE --------- //
   listen(sockfd, 5);
 
-  //-------- CREATE NEW SOCKET TO CONNECT WITH CLIENT -------- //
+  //-------- CREATE NEW SOCKET TO HANDLE CONNECTION WITH CLIENT -------- //
   sockaddr_in newSockAddr;
   socklen_t newSockAddrSize = sizeof(newSockAddr);
+  std::cout << newSockAddrSize << std::endl;
   int newSd = accept(sockfd, (sockaddr *)&newSockAddr, &newSockAddrSize);
-
-  
-
-  return 0;
+  if(newSd<0){
+    std::cout << "Error accepting request from client" << std::endl;
+    return -1;
+  }
+  std::cout << "Connected with client!" << std::endl;
+  while(1)
+    {
+        //receive a message from the client (listen)
+        std::cout << "Awaiting client response..." << std::endl;
+        memset(&msg, 0, sizeof(msg));  //clear the buffer
+        if(!strcmp(msg, "exit")) // if the client types exit the client stops being listened to
+        {
+            std::cout << "Client has quit the session" << std::endl;
+            break;
+        }
+        std::cout << "Client: " << msg << std::endl;
+        std::cout << ">";
+        std::string data;
+        getline(std::cin , data);
+        memset(&msg, 0, sizeof(msg)); //clear the buffer
+        strcpy(msg, data.c_str());
+        if(data == "exit")
+        {
+            //send to the client that server has closed the connection
+            send(newSd, (char*)&msg, strlen(msg), 0);
+            break;
+        }
+        //send the message to client
+    }
+    //we need to close the socket descriptors after we're all done
+    close(newSd);
+    close(sockfd);
+    std::cout << "********Session********" << std::endl;
+   
+    std::cout << "Connection closed..." << std::endl;
+    return 0;   
 }
