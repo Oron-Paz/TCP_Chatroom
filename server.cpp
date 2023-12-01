1a0d7e708555d6ea30e887961d52e4a6df159191
Oron Paz <oron.paz26@gmail.com>
2023-11-30 18:17:53 -0800

    advancing with server side

--- server.cpp
+++ server.cpp
@@ -5,16 +5,38 @@
 #include <sys/socket.h>
 #include <iostream>
 #include <unistd.h>
-#define PORT 8080
+#define PORT 8222 //its my lucky number
 
 int main(int argc, char const* argv[]) {
-  int sockfd = socket(AF_LOCAL, SOCK_STREAM , 0);
-  int bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
+  //buffer for messages to be sent and recieved
+  char msg[1500];
 
+  //------- CREATE THE SOCKET ON THE SERVER SIDE --------- //
+  int sockfd = socket(AF_INET, SOCK_STREAM , 0);
+  if(sockfd < 0) {
+    std::cout << "Error creating the socket" << std::endl;
+    return -1;
+  }
 
-  std::cout << sockfd << std::endl ;
-  std::cout << bind << std::endl ;
+  //------- BIND THE SOCKET TO THE ADRESS OF THE COMPUTER FOR SERVER --------- //
+  struct sockaddr_in addr;
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(PORT);
+  addr.sin_addr.s_addr = INADDR_ANY;
+  if (bind(sockfd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+    std::cout << "Error binding socket" << std::endl;
+    return -1;
+  }
 
+  //------- LISTEN FOR CONNECTIONS FROM CLIENT SIDE --------- //
+  listen(sockfd, 5);
+
+  //-------- CREATE NEW SOCKET TO CONNECT WITH CLIENT -------- //
+  sockaddr_in newSockAddr;
+  socklen_t newSockAddrSize = sizeof(newSockAddr);
+  int newSd = accept(sockfd, (sockaddr *)&newSockAddr, &newSockAddrSize);
+
+  
 
   return 0;
 }
